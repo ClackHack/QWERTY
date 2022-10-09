18ef00067e35065525aacbf129426e850c5d551e
ClackHack <48455071+ClackHack@users.noreply.github.com>
2022-10-09 19:48:14 -0400

    file input

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
+void Error(string message);
+
 int main(int argc, char *argv[]){
-    
+    if (argc != 2){
+        Error("Expected file path argument");
+    }
+    fstream File;
+    File.open(argv[1]);
+
+
+    File.close();
+    return 0;
+}
+
+void Error(string msg){
+    cout << "Error :> " << msg << endl;
 }
